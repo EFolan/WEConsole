612ea2059d7a4ba8fd68995f8590a3dafe19ecd3
Curry <thomas.curry@capgemini.com>
2017-08-03 12:38:38 +0100

    Added query support

--- WEConsole/Program.cs
+++ WEConsole/Program.cs
@@ -15,6 +15,7 @@ namespace WEConsole
             Console.WriteLine("Enter a Table name");
             string TableName = Console.ReadLine();
             Storage.GetTable(TableName);
+            Storage.gettimestamp();
             Storage.getquery();
             Console.WriteLine("Press any key to exit");
             Console.ReadKey();
--- WEConsole/Services/StorageService.cs
+++ WEConsole/Services/StorageService.cs
@@ -35,7 +35,7 @@ namespace WEConsole.Services
                         Console.WriteLine($"Table does not exist. Creating one with name {tableName}");
                     }
                 }
-                catch(Exception e)
+                catch (Exception e)
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine($"Error - {e}");
@@ -45,8 +45,8 @@ namespace WEConsole.Services
                 Console.WriteLine($"Accessed Table: {Table.Name}");
                 return Table;
             }
-        }        
-        public void getquery()
+        }
+        public void gettimestamp()
         {
             try
             {
@@ -62,16 +62,38 @@ namespace WEConsole.Services
                 Console.ForegroundColor = ConsoleColor.Gray;
             }
         }
+        public void getquery()
+        {
+            try
+            {
+                CloudTable table = TableClient.GetTableReference("deviceData");
+                TableQuery<MessageEntity> query = new TableQuery<MessageEntity>().Where(TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, "17380"));
+                foreach (MessageEntity entity in table.ExecuteQuery(query))
+                {
+                    Console.WriteLine("Partition Key: " + entity.PartitionKey + " RowKey: " +entity.RowKey+ " Timestamp: " +entity.Timestamp+ " Message: " +entity.message);
+                    Console.ReadKey();
+                }
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("End of Query");
+                Console.ForegroundColor = ConsoleColor.Gray;
+            }
+            catch(Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Error - {e}");
+                Console.ForegroundColor = ConsoleColor.Gray;
+            }
+        }
     }
     public class MessageEntity : TableEntity
     {
         public MessageEntity(string messsage, string timestamp)
         {
             this.RowKey = timestamp;
-            this.PartitionKey= Message;
+            this.PartitionKey= message;
         }
         public MessageEntity() { }
-        public string Message { get; set; }
+        public string message { get; set; }
         public string TimeStamp { get; set; }
         }
     }
